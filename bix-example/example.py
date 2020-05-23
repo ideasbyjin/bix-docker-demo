39f49895a8411c12ae02201186b4cba8ebe2a814
jleem <jinwoo.leem@gmail.com>
2020-05-23 11:31:44 +0100

    Get residues too

--- bix-example/example.py
+++ bix-example/example.py
@@ -4,5 +4,6 @@ p = PDBParser(QUIET=True)
 s = p.get_structure('3mxw.pdb', '3mxw.pdb')
 
 n_c = len(list(s.get_chains()))
+n_h_r = len([ r for r in s[0]['H'] if r.resname != "HOH" ])
 
-print(f"This protein has {n_c} chains")
+print(f"This protein has {n_c} chains and {n_h_r} residues in chain H")
